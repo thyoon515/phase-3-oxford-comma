fd2dce15fe24abfa8330571d25ece9ce005d6147
thyoon515 <thyoon515@gmail.com>
2022-10-01 21:48:22 -0400

    commit tree 23feead7e6885393fc49a938c66189767c377518

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+    if array.length == 2
+        last_element = array.pop
+        add_and = last_element.split("").unshift(" and ").join
+        array << add_and
+        return array.join
+    elsif array.length >= 3
+        last_element = array.pop
+        add_and = last_element.split("").unshift("and ").join
+        array << add_and
+        return array.join(", ")
+    else array == 1
+        return array.join
+    end
 end
